77b8cb433c3489f5f367f1d2d7ae0b5138c1c4f7
kushwaha9111 <141923972+kushwaha9111@users.noreply.github.com>
2023-08-11 19:29:48 +0530

    Add files via upload

---
+++ NumberGuessing/src/NumberGuessing.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+class NumberGuessing
+{
+    public static void
+    guessingNumberGame()
+    {
+        Scanner sc = new Scanner(System.in);
+        int number = 1 + (int)(100* Math.random());
+        int K = 5;
+        int i, guess;
+        int score=100;
+        String choice = "yes";
+        while(choice.equals("yes"))
+        {
+            System.out.println("-------------------------------------- WELCOME IN NUMBER GUESSING GAME -------------------------------------------------------");
+            System.out.println(" Guess a number  between 1 to 100  within 5 trials.");
+
+            for (i = 0; i < K; i++) {
+
+                System.out.println("Guess the number:-  ");
+                guess = sc.nextInt();
+                if (number == guess) {
+                    System.out.println("Congratulations!!.. You guessed the number.");
+                    System.out.println("Score : "+score);
+                    break;
+                }
+                else if (number > guess
+                        && i != K - 1) {
+                    System.out.println("Opps!! Wrong Guess! The number is greater than " + guess);
+                    score--;
+                }
+                else if (number < guess
+                        && i != K - 1) {
+                    System.out.println("Opps!! Wrong Guess! The number is less than " + guess);
+                    score--;
+                }
+            }
+
+            if (i == K) {
+                System.out.println("You have exhausted all the trials.");
+
+                System.out.println("The number was " + number);
+                System.out.println("Want to play again(Yes/No)?");
+                String ch = sc.next();
+                choice = ch;
+            }
+        }
+    }
+
+    public static void main(String[] args)
+    {
+        guessingNumberGame();
+    }
+}
